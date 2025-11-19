534db093b08a89abaac68f304a2f7672e9d44120
chuong2603 <23130033@st.hcmuaf.edu.vn>
2025-11-20 00:15:02 +0700

    update login, name div

---
+++ JS/login.js
@@ -0,0 +1,7 @@
+function showPage(pageId) {
+    document.querySelectorAll(".page").forEach(p => {
+        p.style.display = "none";
+    });
+
+    document.getElementById(pageId).style.display = "block";
+}
