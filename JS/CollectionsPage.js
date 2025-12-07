2e30d88beba63b05069ea06dde3aa2146da31585
Bùi Thanh Sang <23130268@st.hcmuaf.edu.vn>
2025-12-08 01:41:30 +0700

    this is update for menu Finalize the menu structure and mega menu interactions, improve responsive behavior so the menu stays intact on smaller screens, and adjust typography styles and margins across related components.
    Signed-off-by: Bùi Thanh Sang <23130268@st.hcmuaf.edu.vn>

--- JS/CollectionsPage.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".FilterHeader").forEach(header => {
-    header.addEventListener("click", () => {
-        const content = header.nextElementSibling;
-        content.classList.toggle("active");
-    });
-});
