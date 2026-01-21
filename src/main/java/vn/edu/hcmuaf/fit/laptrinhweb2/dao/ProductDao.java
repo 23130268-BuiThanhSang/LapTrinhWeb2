8d55030517fb8b2a4f00892d2c4d4fc5a75d197d
Bùi Thanh Sang <23130268@st.hcmuaf.edu.vn>
2026-01-21 14:02:16 +0700

    complete product refactor

--- src/main/java/vn/edu/hcmuaf/fit/laptrinhweb2/dao/ProductDao.java
+++ src/main/java/vn/edu/hcmuaf/fit/laptrinhweb2/dao/ProductDao.java
@@ -2,110 +2,76 @@ package vn.edu.hcmuaf.fit.laptrinhweb2.dao;
 
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.core.statement.PreparedBatch;
-import vn.edu.hcmuaf.fit.laptrinhweb2.model.Product;
-import vn.edu.hcmuaf.fit.laptrinhweb2.model.ProductVariant;
-import vn.edu.hcmuaf.fit.laptrinhweb2.model.ProductVariantImage;
+import vn.edu.hcmuaf.fit.laptrinhweb2.enum_macro.GroupType;
+import vn.edu.hcmuaf.fit.laptrinhweb2.model.*;
 
 import java.util.*;
 
-public class ProductDao extends BaseDao{
-    private static final String SQL =
-            "SELECT p.id AS product_id, p.product_name, p.product_type, b.name AS brand_name, p.product_infor, " +
-            "p.product_car_instruction, p.product_return_infor, p.product_gender, " +
-            "v.id AS variant_id, v.color, v.size, v.price, v.stock, "+
-            "i.id AS image_id, i.image_url " +
-            "FROM product p " +
-            "JOIN brand b ON p.brand_id = b.id " +
-            "JOIN product_variant v ON p.id = v.product_id " +
-            "LEFT JOIN product_variant_image i ON v.id = i.variant_id " +
-            "WHERE p.id = :id " +
-            "ORDER BY v.color ASC, v.size ASC, i.id ASC";
-
-
-    static Map<Integer,Product> productMap = new HashMap<Integer,Product>();
+public class ProductDao extends BaseDao {
 
     public List<Product> getAllProduct() {
-        return get().withHandle(h -> h.createQuery("select * from product").mapToBean(Product.class).list());
-    }
-
-    public Product getProductMap(int id) {
-        return productMap.get(id);
+        return null;
     }
 
     public Product getProduct(int id) {
-        return get().withHandle(handle -> {
+        ProductGroupDao groupDao = new ProductGroupDao();
+        ProductTypeDao typeDao = new ProductTypeDao();
 
-            Product product = null;
-            Map<Integer, ProductVariant> variantMap = new LinkedHashMap<>();
+        return get().withHandle(handle -> handle.createQuery("""
+                    SELECT id, product_name, brand_id, product_infor,
+                           product_car_instruction, product_return_infor,
+                           product_gender, product_type_id, collection_id, sport_id
+                    FROM product
+                    WHERE id = :id
+                """).bind("id", id).map((rs, ctx) -> {
+            Product p = new Product();
+            p.setId(rs.getInt("id"));
+            p.setName(rs.getString("product_name"));
+            p.setProductInfor(rs.getString("product_infor"));
+            p.setProductCareInstruction(rs.getString("product_car_instruction"));
+            p.setProductReturnInfor(rs.getString("product_return_infor"));
+            p.setProductGender(rs.getString("product_gender"));
 
-            List<Map<String, Object>> rows = handle.createQuery(SQL)
-                    .bind("id", id)
-                    .mapToMap()
-                    .list();
+            /**
+             * productGroup lấy theo 1 dòng
+             */
+            p.setCollection(groupDao.getProductGroup(GroupType.COLLECTION, rs.getInt("collection_id")));
 
-            for (Map<String, Object> row : rows) {
+            p.setSport(groupDao.getProductGroup(GroupType.SPORT, rs.getInt("sport_id")));
 
-                if (product == null) {
-                    product = new Product(
-                            ((Number) row.get("product_id")).intValue(),
-                            (String) row.get("product_name"),
-                            (String) row.get("product_type"),
-                            (String) row.get("brand_name"),
-                            (String) row.get("product_infor"),
-                            (String) row.get("product_car_instruction"),
-                            (String) row.get("product_return_infor"),
-                            (String) row.get("product_gender"),
-                            new ArrayList<>()
-                    );
-                }
+            p.setBrand(groupDao.getProductGroup(GroupType.BRAND, rs.getInt("brand_id")));
 
-                int variantId = ((Number) row.get("variant_id")).intValue();
+            /**
+             * productType lấy theo 1 dòng
+             */
+            p.setProductType(typeDao.getById(rs.getInt("product_type_id")));
 
-                ProductVariant variant = variantMap.get(variantId);
-                if (variant == null) {
-                    variant = new ProductVariant(
-                            variantId,
-                            (String) row.get("color"),
-                            ((Number) row.get("size")).intValue(),
-                            ((Number) row.get("price")).doubleValue(),
-                            ((Number) row.get("stock")).intValue()
-                    );
-                    variantMap.put(variantId, variant);
-                    product.getVariants().add(variant);
-                }
+            /**
+             * variants lấy theo nhiều dòng
+             */
+            List<ProductVariant> variants = handle.createQuery("""
+                        SELECT id, color, size, price, stock
+                        FROM product_variant
+                        WHERE product_id = :productId
+                    """).bind("productId", p.getId()).mapToBean(ProductVariant.class).list();
 
-                // image (LEFT JOIN nên phải check null)
-                if (row.get("image_id") != null) {
-                    ProductVariantImage img = new ProductVariantImage(
-                            ((Number) row.get("image_id")).intValue(),
-                            (String) row.get("image_url")
-                    );
-                    variant.getImages().add(img);
-                }
+            for (ProductVariant v : variants) {
+                List<ProductVariantImage> images = handle.createQuery("""
+                            SELECT id,
+                                   variant_id AS variantId,
+                                   image_url AS imageUrl
+                            FROM product_variant_image
+                            WHERE variant_id = :variantId
+                        """).bind("variantId", v.getId()).mapToBean(ProductVariantImage.class).list();
+
+                v.setImages(images);
             }
 
-            return product;
-        });
+            p.setVariants(variants);
+
+            return p;
+        }).findOne().orElse(null));
     }
 
-
-
-
-    public void insertProduct(List<Product> products) {
-        Jdbi jdbi= get();
-        jdbi.useHandle(h-> {
-//                                  createUpdate neu luu 1 product
-            PreparedBatch batch = h.prepareBatch("insert into product values (:id,:name,:price,:img)");
-            products.forEach(product -> {
-                batch.bindBean(product).add();
-            });
-            batch.execute();
-        });
-    }
-
-    public static void main(String[] args) {
-        ProductDao dao = new ProductDao();
-        dao.getAllProduct().forEach(System.out::println);
-    }
 
 }
