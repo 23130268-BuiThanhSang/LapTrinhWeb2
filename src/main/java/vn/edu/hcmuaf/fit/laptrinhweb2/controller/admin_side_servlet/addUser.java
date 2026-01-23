7d6eeb0c220656c34d03519f618ee27422713a24
23130344-HuynhGiaTri <23130344@st.hcmuaf.edu.vn>
2026-01-23 21:42:00 +0700

    hash pass, check dupe username/mail when update/add user on admin side
    Signed-off-by: 23130344-HuynhGiaTri <23130344@st.hcmuaf.edu.vn>

--- src/main/java/vn/edu/hcmuaf/fit/laptrinhweb2/controller/admin_side_servlet/addUser.java
+++ src/main/java/vn/edu/hcmuaf/fit/laptrinhweb2/controller/admin_side_servlet/addUser.java
@@ -3,6 +3,8 @@ package vn.edu.hcmuaf.fit.laptrinhweb2.controller.admin_side_servlet;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import vn.edu.hcmuaf.fit.laptrinhweb2.Auth.AuthDao;
+import vn.edu.hcmuaf.fit.laptrinhweb2.Auth.PasswordUtils;
 import vn.edu.hcmuaf.fit.laptrinhweb2.model.Account;
 import vn.edu.hcmuaf.fit.laptrinhweb2.services.AccountService;
 
@@ -11,6 +13,7 @@ import java.io.IOException;
 @WebServlet(name = "addUser", value = "/addUser")
 public class addUser extends HttpServlet {
     private AccountService accountService = new AccountService();
+    private AuthDao authDao = new AuthDao();
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -21,11 +24,37 @@ public class addUser extends HttpServlet {
 
         Account acc = new Account();
         acc.setRole(request.getParameter("accountRole"));
-        acc.setUsername(request.getParameter("username"));
+        //NAME
+        String username = request.getParameter("username");
+        if (!authDao.existsByUsername(username)) {
+            acc.setUsername(username);
+        } else {
+            request.setAttribute("error", "Username duplicate");
+            request.getRequestDispatcher("manageUser_AddUser.jsp").forward(request, response);
+            return;
+        }
         acc.setAccountName(request.getParameter("accountName"));
         acc.setPhoneNumber(request.getParameter("phoneNumber"));
-        acc.setAccountEmail(request.getParameter("accountEmail"));
-        acc.setPassword(request.getParameter("password"));
+        //MAIL
+        String usermail = request.getParameter("accountEmail");
+        if (!authDao.existsByEmail(usermail)) {
+            acc.setAccountEmail(usermail);
+        } else {
+            request.setAttribute("error", "Email duplicate");
+            request.getRequestDispatcher("manageUser_AddUser.jsp").forward(request, response);
+            return;
+        }
+        //PASS
+        String basePass = request.getParameter("password");
+        String password = "";
+        if (PasswordUtils.isStrongPassword(basePass)) {
+            password = PasswordUtils.md5(basePass);
+        } else {
+            request.setAttribute("error", "Password is too weak..");
+            request.getRequestDispatcher("manageUser_AddUser.jsp").forward(request, response);
+            return;
+        }
+        acc.setPassword(password);
         acc.setAccountStatus(Integer.parseInt(request.getParameter("accountStatus")));
         // acc.setAvatarUrl(request.getParameter("avatarUrl")); // placeholder ok
 
